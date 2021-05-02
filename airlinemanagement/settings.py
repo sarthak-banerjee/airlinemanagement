ed9d8749393c3151c8af73d004a379c4c2c9503e
sarthak248 <65854909+sarthak-banerjee@users.noreply.github.com>
2021-05-02 20:28:24 +0530

    v1.9

--- airlinemanagement/settings.py
+++ airlinemanagement/settings.py
@@ -74,12 +74,6 @@ WSGI_APPLICATION = 'airlinemanagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-""" DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-} """
 
 DATABASES = {
 'default': {
